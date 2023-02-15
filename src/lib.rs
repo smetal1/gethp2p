4cfe6c1b139e5b6c4b7e4282e42f9bcb67299f48
smetal1 <saurav.g.kumar@oracle.com>
2023-02-15 21:53:06 +0530

    Bug fixes and comment addition

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ use std::io;
 use web3::transports::Http;
 use web3::Web3;
 
-fn is_valid_node(network_id: &String, our_network_id: &String) -> Result<(), std::io::Error> {
-    // Compare network IDs and protocol versions
-    if our_network_id != network_id {
+// Validation function= This checks for the node id match
+fn is_valid_node(fetch_node_id: &String, expected_node_id: &String) -> Result<(), std::io::Error> {
+    if fetch_node_id != expected_node_id {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
             "Rogue Node Detected",
@@ -15,20 +15,22 @@ fn is_valid_node(network_id: &String, our_network_id: &String) -> Result<(), std
     Ok(())
 }
 
+//get connection object from web3 lib
 fn get_conn(url: &str) -> Result<Web3<Http>, web3::Error> {
     let transport = Http::new(url)?;
     Ok(Web3::new(transport))
 }
 
+//This function get the p2p connection object and extracts the node information
 #[tokio::main]
-pub async fn conn_p2p(our_network_id: String) -> anyhow::Result<()> {
+pub async fn conn_p2p(expected_node_id: String) -> anyhow::Result<()> {
     dotenv().ok();
     let mut choice = String::new();
     let node_endpoint = std::env::var("NODE_ENDPOINT")?;
     let connection_obj = get_conn(&node_endpoint)?;
     loop {
         let node_id = connection_obj.net().version().await?;
-        is_valid_node(&node_id, &our_network_id)?;
+        is_valid_node(&node_id, &expected_node_id)?;
         let is_listening = connection_obj.net().is_listening().await?;
         if is_listening {
             println!("Is Node connected? {is_listening}");
@@ -41,11 +43,9 @@ pub async fn conn_p2p(our_network_id: String) -> anyhow::Result<()> {
             println!("Current chain id: {chain_id}");
             let block_number = connection_obj.eth().block_number().await?;
             println!("Current Block Number: {block_number}" );
-
             println!("Enter 'x' to abort the connection or press any key to fetch new information from connected node");
             choice.clear();
             io::stdin().read_line(&mut choice).unwrap();
-
             if choice.trim() == "x" {
                 println!("P2P handshake ended");
                 break;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use dotenv::dotenv;
 use anyhow;
 use gethp2p::conn_p2p;
-
+//Main function
 pub fn main()  -> anyhow::Result<()>{
     dotenv().ok();
     let node_id=std::env::var("NODE_ID")?;
     conn_p2p(node_id)?;
     Ok(())
-
-
 }
 
